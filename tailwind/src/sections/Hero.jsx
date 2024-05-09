43d44826488fd2b6e3cdcb3e7991c6b2f967734a
Awiral Chand <103342572+awi246@users.noreply.github.com>
2024-05-09 10:59:46 +0545

    big shoe

--- tailwind/src/sections/Hero.jsx
+++ tailwind/src/sections/Hero.jsx
@@ -1,23 +1,70 @@
 import { arrowRight } from "../assets/icons";
 import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
+
+import { statistics, shoes } from "../constants/index";
+import { bigShoe1 } from "../assets/images";
+import CountUp from "react-countup";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col min-h-screen gap-10 max-container  padding-x"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 "></div>
-      <p className="">Our Summer Collection</p>
-      <h1>
-        <span>The New Arrival</span>
-        <br />
-        <span>Nike</span>
-        Shoes
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button label="show now" iconURL={arrowRight}/>
-      </h1>
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike </span>
+          Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt6 mb-14">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
 
+        <Button label="show now" className="w-1/2" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
+          {statistics.map((stats, index) => (
+            <div key={index}>
+              <p className="text-4xl font-palanquin font-bold">
+                <CountUp end={stats.value} duration={1} />+
+              </p>
+              <p className="font-montserrat leading-7 text-slate-gray ">
+                {stats.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImg}
+          alt=""
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] max-sm:px-6  ">
+          {shoes.map((shoe, index) => (
+            <div key={index}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
